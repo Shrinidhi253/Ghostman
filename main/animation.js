b0ac93fce88c87409baac6181f0ce25400267ffe
Shrinidhi253 <shrinidhi.ganapathi@gmail.com>
2025-10-20 11:21:27 +0200

    Added the caged ghosts

--- main/animation.js
+++ main/animation.js
@@ -20,7 +20,7 @@ export class PumpkinPatch {
 
             for (let j = 0; j < this.cols; j++) {
                 let col = document.createElement("td");
-                col.id = `${i}-${j}`;
+                col.id = `PumpkinPatch(${i},${j})`;
                 row.appendChild(col);
             }
             this.grid.appendChild(row);
@@ -31,9 +31,34 @@ export class PumpkinPatch {
         for (let i = 0; i < numPumpkins; i++) {
             var rowInd = Math.floor(i / this.rows);
             var colInd = i % this.cols;
-            var cell = document.getElementById(`${rowInd}-${colInd}`)
+            var cell = document.getElementById(`PumpkinPatch(${rowInd},${colInd})`)
             cell.innerHTML = '<img src="/images/pumpkin.svg">'
         }
     }
 
+}
+
+export class Ghosts {
+    ghosts = document.querySelector(".ghosts");
+    grid = document.createElement("table");
+    rows = 6;
+    cols = 1;
+
+    main() {
+        this.createCages();
+        this.ghosts.appendChild(this.grid);
+    }
+
+    createCages() {
+        for (let i = 0; i < this.rows; i++) {
+            let row = document.createElement("tr");
+            let col = document.createElement("td");
+
+            col.id = `Ghosts(${i},0)`;
+            col.innerHTML = '<img src="/images/trapped_ghost.svg">'
+
+            row.appendChild(col);
+            this.grid.appendChild(row);
+        }
+    }
 }
